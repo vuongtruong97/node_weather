e08cc7c314582b8352495a14b33b10101a372d29
Vương Trương <truongquocvuongnb@gmail.com>
2022-05-07 02:16:42 +0700

    update weather icon

--- public/js/home.js
+++ public/js/home.js
@@ -29,7 +29,11 @@ searchBtn.addEventListener('click', () => {
                     if (data.error) {
                         return (weatherDetails.innerHTML = `<h2>${data.error}</h2>`)
                     }
-                    weatherDetails.innerHTML = `<h2>Weather at :<b> ${data.location}</b></h2>
+                    weatherDetails.innerHTML = `
+                    <div class='weather_title'>
+                    <h2>Weather at :<b> ${data.location}</b></h2>
+                    <img src='http://openweathermap.org/img/wn/${data.weather.icon}@2x.png' />
+                    </div>
                                             <ul>
                                                 <li>Temperature:<b> ${data.temp}  °C</b></li>
                                                 <li>Humidity:<b> ${data.humidity} %</b></li>
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,7 +3,6 @@ const { API_KEY, API_UNIT } = process.env
 
 async function forecast(longitude, latitude) {
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&lang=en&appid=${API_KEY}&units=metric`
-    console.log(url)
     const weather = await fetch(url)
         .then((res) => {
             if (res.status >= 400 && res.status < 600) {
